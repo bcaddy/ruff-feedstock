f914c34eb61a91558c998a86d46eebf7b5474fe0
Bob Caddy <rcaddy@princeton.edu>
2024-10-02 13:16:27 -0400

    Fix for 64KB page aarch systems

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,6 +3,10 @@ set -ex
 # https://github.com/rust-lang/cargo/issues/10583#issuecomment-1129997984
 export CARGO_NET_GIT_FETCH_WITH_CLI=true
 
+if [[ "${target_platform}" == "linux-aarch64" ]]; then
+  export JEMALLOC_SYS_WITH_LG_PAGE=16
+fi
+
 pushd crates/ruff
 # Bundle all downstream library licenses
 cargo-bundle-licenses \
